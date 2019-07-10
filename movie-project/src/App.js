3d430324c20c8fe3e58010ed366f0fa98d72ef3d
LeeYongwon <dyddnjs0982@naver.cpm>
2019-07-10 22:05:44 +0900

    Movie Component 생성

--- movie-project/src/App.js
+++ movie-project/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-
+import Moive from './Movie';
 import './App.css';
 
 class App extends Component{
   render(){
     return(
       <div className='App'>
-        <h1>당연하지.</h1>
+        <Moive />
       </div>
     )
   }
---
+++ movie-project/src/Movie.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Movie extends Component{
+    render(){
+        return (
+            <div>이것은 Movie Component입니다.</div>
+        );
+    }
+}
+
+export default Movie;
