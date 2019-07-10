b462f7f0ed8c9c045dd72a4a468894f26285918e
LeeYongwon <dyddnjs0982@naver.cpm>
2019-07-10 23:16:54 +0900

    app.js에 moviesTitles,movieImage 데이터 생성// 이 정보들을 Movie Component에 전달// app Component에서 전달받은 데이터를 Movie Compoent에서 this.props.title 사용// Movie Component에서 전달 받은 props.poster를 MoviePoster Component에 전달//

--- movie-project/src/Movie.js
+++ movie-project/src/Movie.js
@@ -1,9 +1,27 @@
 import React, {Component} from 'react';
+import './Movie.css';
 
 class Movie extends Component{
     render(){
+        console.log(`Movie에서의 ${this.props.poster}`);
         return (
-            <div>이것은 Movie Component입니다.</div>
+            <div>
+                <h1>{`이 영화의 제목은 ${this.props.title}`}</h1>
+                <Movieposter poster={this.props.poster} />
+            </div>
+            
+        );
+    }
+}
+
+class Movieposter extends Component{
+    
+    render(){
+        console.log(`MoviePoseter에서의 ${this.props.poster}`);
+        return(
+            <div>
+               <img src={this.props.poster}></img>
+            </div>
         );
     }
 }
