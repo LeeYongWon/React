4d43105a430f0ea04707787f1f5811c977626e74
LeeYongwon <dyddnjs0982@naver.cpm>
2019-07-15 16:17:59 +0900

    외부 데이터를 state를 통해 Component 내부에 넣는다

--- movie-project/src/App.js
+++ movie-project/src/App.js
@@ -2,25 +2,6 @@ import React, {Component} from 'react';
 import Moive from './Movie';
 import Movie2 from './Movie2';
 import './App.css';
-const movies =[
-  {
-    title:'Matrix',
-    poster:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVn0G9GwGIx2o8_XCNvXjslCsn-okXRmI--J99_mLfbJslqCdW',
-    
-  },
-  {
-    title:'Full Metal jacket',
-    poster:"https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR85,0,630,1200_AL_.jpg"
-  },
-  {
-    title:"Oldboy",
-    poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title:"Star Wars",
-    poster:"https://starwarsblog.starwars.com/wp-content/uploads/2019/01/SWCO19-artists-key-art-tall-B.jpg"
-  }
-]
 
 class App extends Component{
   /*
@@ -42,28 +23,45 @@ class App extends Component{
     
   }
   */
+ //state를 만드는 방법
+ 
+ state ={
+   greeting:'state greeting',
+   movies:[
+    {
+      title:'Matrix',
+      poster:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVn0G9GwGIx2o8_XCNvXjslCsn-okXRmI--J99_mLfbJslqCdW',
+      
+    },
+    {
+      title:'Full Metal jacket',
+      poster:"https://m.media-amazon.com/images/M/MV5BNzkxODk0NjEtYjc4Mi00ZDI0LTgyYjEtYzc1NDkxY2YzYTgyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY1200_CR85,0,630,1200_AL_.jpg"
+    },
+    {
+      title:"Oldboy",
+      poster:"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+    },
+    {
+      title:"Star Wars",
+      poster:"https://starwarsblog.starwars.com/wp-content/uploads/2019/01/SWCO19-artists-key-art-tall-B.jpg"
+    }
+  ]
+ }
 
+ componentDidMount(){
 
-  
-
-  componentWillMount(){
-    console.log('will mount')
-  }
-
-  componentDidMount(){
-    console.log('did mount')
-  }
+ }
   render(){
     console.log('did render')
     return(
       <div className='App'>
         <div className='Movie'>
-          {movies.map((data, index) =>{
+          {this.state.movies.map((data, index) =>{
             return <Moive title={data.title} poster={data.poster} key={index+1} />
             })};
         </div>
         <div className='Movie2'>
-            {movies.map((moviedata, index)=>{
+            {this.state.movies.map((moviedata, index)=>{
               return <Movie2 title={moviedata.title} poster ={moviedata.poster} key={index+1} />
             })};
         </div>
